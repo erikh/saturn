8a206d0b8199b5d7fc9ebc019c443e4e7413649a
Erik Hollensbe <git@hollensbe.org>
2023-08-06 21:42:24 -0700

    A script I wrote so I can actually use it while I'm hacking on it
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

---
+++ swap_db.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+if [ $# -ne 1 ]
+then
+  echo >&2 "usage: $0 [save|restore]"
+  exit 1
+fi
+
+command=$1
+
+case $command in
+  clear)
+    rm ~/.saturn.db
+  ;;
+  restore)
+    mv ~/.saturn.db.save ~/.saturn.db
+  ;;
+  save)
+    if [ -f ~/.saturn.db.save ]
+    then
+      echo >&2 "Save file already exists"
+      exit 1
+    fi
+    mv ~/.saturn.db ~/.saturn.db.save
+  ;;
+esac
